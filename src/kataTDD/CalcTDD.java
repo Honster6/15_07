95c0b9d42a9d10c3af410224e05c35cb71440b91
Sebastian <40136322+Honster6@users.noreply.github.com>
2018-07-21 11:29:56 +0200

    TDD

---
+++ src/kataTDD/CalcTDD.java
@@ -0,0 +1,21 @@
+package kataTDD;
+
+public class CalcTDD {
+
+    public int add(String numbers) {
+        if (numbers.equals("")) {
+            return 0;
+        } else {
+            String[] parts = numbers.split(",");
+            int sum = 0;
+            for (int i = 0; i < parts.length; i++) {
+                int x = Integer.parseInt(parts[i]);
+                sum = sum + x;
+            }
+            return sum;
+
+
+        }
+    }
+}
+
---
+++ src_test/kataTDD/CalcTDDTest.java
@@ -0,0 +1,40 @@
+package kataTDD;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTDDTest {
+
+    @Test
+    public void shouldReturnZeroForEmptyString() {
+        // given
+        CalcTDD calculator = new CalcTDD();
+        // when
+        int result = calculator.add("");
+        // then
+        assertEquals(0, result);
+    }
+    @Test
+    public void shouldReturnOneNumber() {
+        // given
+        CalcTDD calculator = new CalcTDD();
+        // when
+        int result = calculator.add("2");
+        // then
+        assertEquals(2, result);
+    }
+    @Test
+    public void shouldReturnSumForTwoNumbers() {
+        // given
+        CalcTDD calculator = new CalcTDD();
+        // when
+        int result = calculator.add("3,2,4,5");
+        // then
+        assertEquals(14, result);
+    }
+
+
+
+
+}
